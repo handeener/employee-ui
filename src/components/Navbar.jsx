264b08d10fad03386b8a22e1865e5fff8525385a
handeener <hande.bucaga@gmail.com>
2026-01-17 18:33:24 +0300

    navbar and index.css changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav style ={{ padding: '10px', background: "#222", marginBottom: "20px" }}>
-            <Link to="/" style={{ color: "#fff", marginRight: '20px' }}>Employee List</Link>
-            <Link to="/add" style={{ color: "#fff" }}>Add Employee</Link>
+        <nav>
+            <Link to="/">Employee List</Link>
+            <Link to="/add">Add Employee</Link>
         </nav>
     );
 }
